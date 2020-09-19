967bd753d4a6ff945c51d6e850507f11dea29ce9
Kristi Miller <kristi.ann.miller@gmail.com>
2020-09-19 15:12:39 -0600

    Add datasets for test use

---
+++ test/datasets/trips-data.js
@@ -0,0 +1,34 @@
+let tripsData = {
+  "trips": [
+    {
+      "id": 1,
+      "userID": 44,
+      "destinationID": 1,
+      "travelers": 1,
+      "date": "2019/09/16",
+      "duration": 8,
+      "status": "approved",
+      "suggestedActivities": []
+    },
+    {
+      "id": 2,
+      "userID": 1,
+      "destinationID": 2,
+      "travelers": 5,
+      "date": "2020/10/04",
+      "duration": 18,
+      "status": "pending",
+      "suggestedActivities": []
+    },
+    {
+      "id": 3,
+      "userID": 1,
+      "destinationID": 3,
+      "travelers": 4,
+      "date": "2020/05/22",
+      "duration": 17,
+      "status": "approved",
+      "suggestedActivities": []
+    }
+  ]
+}
