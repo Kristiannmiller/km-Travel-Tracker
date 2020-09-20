320c453ba8b703a0947ca10ebe19b52b03a2b878
Kristi Miller <kristi.ann.miller@gmail.com>
2020-09-19 20:07:10 -0600

    Add functionality to determineTripStatus method

--- src/trip.js
+++ src/trip.js
@@ -6,23 +6,34 @@ class Trip {
     this.travelerID = tripData.userID
     this.destinationID = tripData.destinationID
     this.totalTravelers = tripData.travelers
-    this.departDate = tripData.date
+    this.departDate = moment(new Date(tripData.date)).format('YYYY/MM/DD')
     this.duration = tripData.duration
     this.status = tripData.status
     this.suggestedActivities = tripData.suggestedActivities
   }
-  
+
   dateRange() {
-    let startDate = moment(new Date(this.departDate)).format('YYYY/MM/DD')
     let range = new Array(this.duration).fill(0)
     return range.map((day, i) => {
-      return day = moment(new Date(startDate)).add(i, 'days').format('YYYY/MM/DD')
+      return day = moment(new Date(this.departDate)).add(i, 'days').format('YYYY/MM/DD')
     })
   }
 
-  determineTripStatus(date) {
+  determineTripStatus(currentDate) {
+    let compareDate = moment(new Date(currentDate)).format('YYYY/MM/DD')
+    let range = this.dateRange()
+    let endDate = range[range.length - 1]
     if (this.status === 'pending') {
       return 'pending'
+    }
+    if (range.includes(compareDate)) {
+      return 'present'
+    }
+    if (moment(new Date(compareDate)).isAfter(moment(new Date(endDate)), 'day')) {
+      return 'past'
+    }
+    if (moment(new Date(compareDate)).isBefore(moment(new Date(this.departDate)), 'day')) {
+      return 'upcoming'
     }
   }
 }
