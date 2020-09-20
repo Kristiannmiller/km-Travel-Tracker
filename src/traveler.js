a9a8a05a21a7b43472f7942a34d3b704520bb7a9
Kristi Miller <kristi.ann.miller@gmail.com>
2020-09-19 23:26:07 -0600

    Add functionality to determineTrips method

--- src/traveler.js
+++ src/traveler.js
@@ -1,12 +1,41 @@
+import Trip from '../src/trip.js';
+
 class Traveler {
   constructor(travelerData) {
     this.id = travelerData.id
     this.name = travelerData.name
     this.travelerType = travelerData.travelerType
+    this.allTrips = []
     this.pastTrips = []
     this.futureTrips = []
     this.pendingTrips = []
-    this.currentLocation = {destination: "Denver, Colorado"}
+    this.currentLocation = "Denver, Colorado"
+  }
+  determineTrips(trips, currentDate, allDestinations) {
+    trips.forEach(trip => {
+      let tripDestination
+      allDestinations.forEach(destination => {
+        if(trip.destinationID === destination.id)
+        tripDestination = destination
+      })
+      if (trip.userID === this.id) {
+        this.allTrips.push(new Trip(trip, tripDestination))
+      }
+    })
+    this.sortTrips(currentDate)
+  }
+  sortTrips(currentDate) {
+    this.allTrips.forEach(trip => {
+      if (trip.determineTripStatus(currentDate) === 'pending') {
+        this.pendingTrips.push(trip)
+      } else if (trip.determineTripStatus(currentDate) === 'past') {
+        this.pastTrips.push(trip)
+      } else if (trip.determineTripStatus(currentDate) === 'upcoming') {
+        this.futureTrips.push(trip)
+      } else if (trip.determineTripStatus(currentDate) === 'present') {
+        this.currentLocation = trip.destinationData.destination
+      }
+    })
   }
 }
 export default Traveler;
