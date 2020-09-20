f342bae80868b6542463e7ae0804667db3918add
Kristi Miller <kristi.ann.miller@gmail.com>
2020-09-19 23:24:57 -0600

    Update dataset to reflect destination ids for use in tests

--- test/datasets/trips-data.js
+++ test/datasets/trips-data.js
@@ -15,7 +15,7 @@ let tripsData = {
       "userID": 1,
       "destinationID": 2,
       "travelers": 5,
-      "date": "2020/10/04",
+      "date": "2020/10/20",
       "duration": 18,
       "status": "approved",
       "suggestedActivities": []
@@ -32,10 +32,50 @@ let tripsData = {
     },
     {
       "id": 4,
-      "userID": 3,
-      "destinationID": 12,
+      "userID": 1,
+      "destinationID": 1,
       "travelers": 6,
       "date": "2020/12/6",
+      "duration": 16,
+      "status": "pending",
+      "suggestedActivities": []
+    },
+    {
+      "id": 5,
+      "userID": 3,
+      "destinationID": 2,
+      "travelers": 1,
+      "date": "2018/09/16",
+      "duration": 8,
+      "status": "approved",
+      "suggestedActivities": []
+    },
+    {
+      "id": 2,
+      "userID": 1,
+      "destinationID": 3,
+      "travelers": 5,
+      "date": "2019/01/04",
+      "duration": 18,
+      "status": "approved",
+      "suggestedActivities": []
+    },
+    {
+      "id": 3,
+      "userID": 1,
+      "destinationID": 3,
+      "travelers": 4,
+      "date": "2020/02/19",
+      "duration": 17,
+      "status": "approved",
+      "suggestedActivities": []
+    },
+    {
+      "id": 4,
+      "userID": 1,
+      "destinationID": 2,
+      "travelers": 6,
+      "date": "2020/04/06",
       "duration": 16,
       "status": "pending",
       "suggestedActivities": []
